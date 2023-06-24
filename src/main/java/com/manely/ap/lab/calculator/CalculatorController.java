4a2ebaec00147e6ea5d15d1b3cb87c1809e487c1
Manely Hamedani <manely.hamedani@icloud.com>
2023-06-24 05:45:29 +0330

    View Completed.

---
+++ src/main/java/com/manely/ap/lab/calculator/CalculatorController.java
@@ -0,0 +1,164 @@
+package com.manely.ap.lab.calculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+
+public class CalculatorController {
+
+    @FXML
+    private GridPane root;
+
+    @FXML
+    private Button ACButton;
+
+    @FXML
+    private TextField displayTextField;
+
+    @FXML
+    private Button divisionButton;
+
+    @FXML
+    private Button dotButton;
+
+    @FXML
+    private Button eightButton;
+
+    @FXML
+    private Button equalButton;
+
+    @FXML
+    private Button fiveButton;
+
+    @FXML
+    private Button fourButton;
+
+    @FXML
+    private Button minusButton;
+
+    @FXML
+    private Button multiplyButton;
+
+    @FXML
+    private Button nineButton;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button plussButton;
+
+    @FXML
+    private Button sevenButton;
+
+    @FXML
+    private Button sixButton;
+
+    @FXML
+    private Button threeButton;
+
+    @FXML
+    private Button twoButton;
+
+    @FXML
+    private Button zeroButton;
+
+    @FXML
+    void ACButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void divisionButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dotButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void eightButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void equalButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fiveButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void fourButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void keyPressed(KeyEvent event) {
+
+    }
+
+    @FXML
+    void minusButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void multiplyButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nineButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oneButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void plusButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sevenButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sixButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void threeButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void twoButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void zeroButtonPressed(ActionEvent event) {
+
+    }
+
+
+    public void initialize() {
+        root.setStyle("-fx-font-family: 'Apple Braille'");
+    }
+
+}
